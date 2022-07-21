78e0f5670801d69f0ba425b939e47eb48cb871e5
tatsuma-wakabayashi <60612975+tatsuma-wakabayashi@users.noreply.github.com>
2022-07-21 10:33:57 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+const list = document.getElementsByClassName("list");
+const [...arr1] = list;
+
+arr1.map((value) => {
+  value.style = "color: red;";
+  return console.log("end");
+});
