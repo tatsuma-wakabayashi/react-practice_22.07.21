05da6e160bcb1d6786c7eb70290980f5a681e93c
tatsuma-wakabayashi <60612975+tatsuma-wakabayashi@users.noreply.github.com>
2022-07-21 10:40:43 +0900

    0721　スプレッド構文&マップ domで取得 分割代入で配列に スプレッド構文で結合して mapで繰り返し処理

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import "./styles.css";
 const list = document.getElementsByClassName("list");
 const [...arr1] = list;
 
-arr1.map((value) => {
-  value.style = "color: red;";
-  return console.log("finish");
-});
+arr1.map((value) => (value.style = "color:red"));
