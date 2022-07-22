850236b11dcd087af0270415e130ae3166e6d9e3
tatsuma-wakabayashi <60612975+tatsuma-wakabayashi@users.noreply.github.com>
2022-07-22 10:53:41 +0900

    map練習 DOMでとってきて 分割代入&スプレッド構文で配列にして マップで繰り返し処理（ 配列の中のhtmlの文字列を取り出してそれを配列にする ）

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./styles.css";
 
-const list = document.getElementsByClassName("list");
+const list = document.getElementsByTagName("li");
 const [...arr1] = list;
 
-arr1.map((value) => (value.style = "color:red"));
+const newArr = arr1.map((value) => {
+  return value.textContent;
+});
+
+console.log(newArr);
